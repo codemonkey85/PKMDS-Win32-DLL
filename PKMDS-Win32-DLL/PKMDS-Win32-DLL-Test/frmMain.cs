387061c4e2a55cc5e497bb64f4a46013d2ea4d9f
Michael Bond <codemonkey85@gmail.com>
2014-03-09 11:25:06 -0400

    Post-build copy, build config-based dependency

--- PKMDS-Win32-DLL/PKMDS-Win32-DLL-Test/frmMain.cs
+++ PKMDS-Win32-DLL/PKMDS-Win32-DLL-Test/frmMain.cs
@@ -13,8 +13,8 @@ namespace PKMDS_Win32_DLL_Test
     public partial class frmMain : Form
     {
 
-        public string dbfile = "C:\\Users\\michaelbond\\Dropbox\\PKMDS Databases\\veekun-pokedex.sqlite";
-        public string savefile = "C:\\Users\\michaelbond\\Dropbox\\Saves\\Mike B Sav.sav";
+        public string dbfile = "C:\\Users\\Michael Bond\\Dropbox\\PKMDS Databases\\veekun-pokedex.sqlite";
+        public string savefile = "C:\\Users\\Michael Bond\\Dropbox\\Saves\\Mike B2 Sav.sav";
 
         public frmMain()
         {
@@ -23,10 +23,16 @@ namespace PKMDS_Win32_DLL_Test
         private void btnTest_Click(object sender, EventArgs e)
         {
             //lblTest.Text = GetPKMName_FromSav(savefile, 0, 0, dbfile); // GetPKMName((int)(numSpecies.Value), 9, dbfile);
+            this.Text = PKMDS.GetTrainerName_FromSav(savefile);
             cbBox.Items.Clear();
             for (int box = 0; box < 24; box++)
             {
+                //MessageBox.Show(PKMDS.GetBoxName(savefile, box));
                 cbBox.Items.Add(PKMDS.GetBoxName(savefile, box));
+            }
+            if (cbBox.Items.Count > 0) 
+            {
+                cbBox.SelectedIndex = 0;
             }
         }
 
@@ -34,8 +40,8 @@ namespace PKMDS_Win32_DLL_Test
         {
             if (cbBox.Items.Count > 0)
             {
-                //lblTest.Text = PKMDS.GetPKMName_FromSav(savefile, cbBox.SelectedIndex, (int)(numSlot.Value - 1), dbfile);
-                lblTest.Text = PKMDS.GetMoveName(1, 9, dbfile);
+                lblTest.Text = PKMDS.GetPKMName_FromSav(savefile, cbBox.SelectedIndex, (int)(numSlot.Value - 1), dbfile);
+                //lblTest.Text = PKMDS.GetMoveName(1, 9, dbfile);
                 //lblTest.Text = GetTrainerName_FromSav(savefile);
                 //GetPKMStat(savefile, cbBox.SelectedIndex, (int)(numSlot.Value - 1), Convert.ToInt16(stats.HP), dbfile).ToString()
                 //+ '\n' + GetPKMStat(savefile, cbBox.SelectedIndex, (int)(numSlot.Value - 1), Convert.ToInt16(stats.Attack), dbfile).ToString()
