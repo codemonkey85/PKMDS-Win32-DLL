f2f85f40c72dfc2b8327945212379f5ae70d9be5
Michael Bond <codemonkey85@gmail.com>
2014-04-09 00:34:14 -0400

    Reworked functions to fit around managed structs

--- PKMDS-Win32-DLL/PKMDS-Win32-DLL/PKMDS-Win32-DLL.cpp
+++ PKMDS-Win32-DLL/PKMDS-Win32-DLL/PKMDS-Win32-DLL.cpp
@@ -17,6 +17,21 @@ EXPORT void CloseDB()
 	closedb();
 }
 
+EXPORT void GetSAVData_INTERNAL(bw2sav_obj * save, const char * savefile)
+{
+	read(savefile,save);
+};
+
+EXPORT void GetPKMData_INTERNAL(pokemon_obj * pokemon, bw2sav_obj * sav, int box, int slot)
+{
+	pokemon_obj * pkm = &(sav->cur.boxes[box].pokemon[slot]);
+	if(!(pkm->isboxdatadecrypted))
+	{
+		decryptpkm(pkm);
+	}
+	*pokemon = *pkm;
+};
+
 EXPORT BSTR GetPKMName(int speciesid, int langid)
 {
 	std::string ret = lookuppkmname(speciesid,langid).c_str();
@@ -35,66 +50,51 @@ EXPORT BSTR GetMoveName(int moveid, int langid)
 	return ANSItoBSTR(ret.c_str());
 }
 
-EXPORT BSTR GetPKMName_FromSav(const char * savefile, int box, int slot)
+EXPORT BSTR GetPKMName_FromObj/*_FromSav*/(pokemon_obj * pkm/*, int box, int slot*/)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
-	pokemon_obj * pkm = &(sav->cur.boxes[box].pokemon[slot]);
-	decryptpkm(pkm);
+	//pokemon_obj * pkm = &(sav->cur.boxes[box].pokemon[slot]);
+	//if(!(pkm->isboxdatadecrypted))
+	//{
+	//	decryptpkm(pkm);
+	//}
 	std::string ret = lookuppkmname(pkm);
 	return ANSItoBSTR(ret.c_str());
 }
 
-EXPORT BSTR GetTrainerName_FromSav(const char * savefile)
+EXPORT BSTR GetTrainerName_FromSav(bw2sav_obj * sav)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
-	std::wstring trainername = getwstring(sav->cur.trainername); // sav->cur.party.pokemon[0].otname;
+	std::wstring trainername = getwstring(sav->cur.trainername);
 	std::string trainernamestr = std::string(trainername.begin(),trainername.end());
 	return ANSItoBSTR(trainernamestr.c_str());
 }
 
-EXPORT int GetTrainerTID_FromSav(const char * savefile)
+EXPORT int GetTrainerTID_FromSav(bw2sav_obj * sav)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
 	return sav->cur.tid;
 }
 
-EXPORT int GetTrainerSID_FromSav(const char * savefile)
+EXPORT int GetTrainerSID_FromSav(bw2sav_obj * sav)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
 	return sav->cur.sid;
 }
 
-EXPORT BSTR GetBoxName(const char * savefile, int box)
+EXPORT BSTR GetBoxName(bw2sav_obj * sav, int box)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
 	std::wstring boxname = getwstring(sav->cur.boxnames[box]);
 	std::string boxstr = std::string(boxname.begin(),boxname.end());
 	return ANSItoBSTR(boxstr.c_str());
 }
 
-EXPORT int GetPKMStat(const char * savefile, int box, int slot, int stat)
+EXPORT int GetPKMStat(bw2sav_obj * sav, int box, int slot, int stat)
 {
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
 	pokemon_obj * pkm = &(sav->cur.boxes[box].pokemon[slot]);
-	decryptpkm(pkm);
+	if(!(pkm->isboxdatadecrypted))
+	{
+		decryptpkm(pkm);
+	}
 	int ret = getpkmstat(pkm,Stat_IDs::stat_ids(stat));
 	return ret;
 }
-
-EXPORT void GetPKMData_INTERNAL(pokemon_obj * pokemon, const char * savefile, int box, int slot)
-{
-	bw2sav_obj * sav = new bw2sav_obj();
-	read(savefile,sav);
-	pokemon_obj * pkm = &(sav->cur.boxes[box].pokemon[slot]);
-	decryptpkm(pkm);
-	*pokemon = *pkm;
-};
 
 BSTR ANSItoBSTR(const char* input)
 {
